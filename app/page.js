d303b9665df09c298d79fc5e9b73a9fb00df3ab3
emperorjwl <jeweladegor21@gmail.com>
2024-12-28 09:49:37 +0100

    empymusicapp

--- app/page.js
+++ app/page.js
@@ -1,95 +1,118 @@
+"use client";
+import React, { useState, useEffect, use } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { Howl } from "howler";
 
-export default function Home() {
+const song = new Howl({ src: ["/anoti-audio.mp3"] });
+
+export default function MusicPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    if (isPlaying) {
+      song.pause();
+    } else {
+      song.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        const currentTime = song.seek();
+        const totalTime = song.duration();
+        const percentage = (currentTime / totalTime) * 100;
+        setProgress(percentage || 0);
+      }, 500);
+    }
+
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="now-playing">
+      <div className="albumart">
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/wizkidalbumart.jpg"
+          alt="Wizkid Album Art"
+          width={300}
+          height={300}
+          className="albumart-img"
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <div className="album-info">
+        <div className="artist">
+          <div className="album-title">
+            <div className="track-title">
+              <p>Anoti </p>
+            </div>
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/explicit.png"
+              height={15}
+              width={15}
+              alt="explicit"
+              className="explicit-icon"
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="artist-album">
+            <div className="artist-name">
+              <p>Wizkid </p>
+            </div>
+            <span> — </span>
+            <div className="album-name">
+              <p> Made In Lagos: Deluxe Edition</p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="favorite">
+          <Image src="/star.png" height={20} width={20} alt="favorite" />
+          <Image src="/threedots.png" height={20} width={20} alt="more" />
+        </div>
+      </div>
+
+      <div className="progress-container">
+        <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+      </div>
+
+      <div className="controls">
+        <div className="playing-next-img">
+          <Image src="/menu-bar.png" height={20} width={20} alt="menu-bar" />
+        </div>
+
+        <div className="actual-controls">
+          <div className="rewind">
+            <Image src="/rewind.png" height={20} width={20} alt="rewind" />
+          </div>
+
+          <div className="play" onClick={togglePlay}>
+            <Image
+              src={isPlaying ? "/pause.png" : "/play.png"}
+              height={10}
+              width={10}
+              alt="play-pause"
+            />
+          </div>
+
+          <div className="forward">
+            <Image
+              src="/fast-forward.png"
+              height={20}
+              width={20}
+              alt="forward"
+            />
+          </div>
+        </div>
+
+        <div className="lyrics-img">
+          <Image src="/lyrics.png" height={25} width={25} alt="lyrics" />
+        </div>
+      </div>
     </div>
   );
 }
